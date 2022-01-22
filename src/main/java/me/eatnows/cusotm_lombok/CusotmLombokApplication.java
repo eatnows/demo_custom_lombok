26373b681c85750a665594cf620f18b3e33e683d
eatnows <gagyasindev@gmail.com>
2022-01-22 17:14:19 +0900

    initial commit

---
+++ src/main/java/me/eatnows/cusotm_lombok/CusotmLombokApplication.java
@@ -0,0 +1,13 @@
+package me.eatnows.cusotm_lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusotmLombokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CusotmLombokApplication.class, args);
+    }
+
+}
